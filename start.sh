91433e3076106c12ed1c1634ce786b8aaa4b3802
Bigby <bartosz.lesniatynski@gmail.com>
2020-03-13 00:29:52 +0100

    Add pid file output

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 gradle build
-java -jar build/libs/rtask-0.0.1-SNAPSHOT.jar & (cd ../rtask-angular ; ng serve)
+java -jar build/libs/rtask-0.0.1-SNAPSHOT.jar & echo $! > ./pid.file & (cd ../rtask-angular ; ng serve --open & echo $! > ./pid.file)
